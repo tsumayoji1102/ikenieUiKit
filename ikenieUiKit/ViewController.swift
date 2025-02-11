18561ed90f9a21c9dc75099f0083cfe014615b7e
塩見陵介 <ryosukesukairain1@gmail.com>
2025-02-11 23:01:22 +0900

    assetIdentiferが取得できるか実験

--- ikenieUiKit/ViewController.swift
+++ ikenieUiKit/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
+    
+    private var selectphotoButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        createSelectPhotoButton()
+        view.addSubview(selectphotoButton)
     }
+    
+    func createSelectPhotoButton() {
+        selectphotoButton = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        selectphotoButton.backgroundColor = .systemBlue
+        selectphotoButton.tintColor = .black
+        selectphotoButton.setTitle("Select Photo", for: .normal)
+        selectphotoButton.addTarget(self, action: #selector(selectPhoto), for: .touchUpInside)
+    }
+        
+    
+    @objc func selectPhoto() {
+        let config = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+}
 
+// MARK: - PHPickerViewControllerDelegate
+extension ViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        print("results: \(results)")
+        for result in results {
+            let identifier = result.assetIdentifier
+            print("identifier: \(identifier!)")
+        }
+    }
 
 }
 
